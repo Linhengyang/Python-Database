d6bbd63ee1fa4c6cb6c2f92fc1b75555f068ee6f
Lin, Aaron (10170) <aaron.lin@protiviti.com>
2020-12-12 20:55:58 -0500

    main

--- Code/sqlite.py
+++ Code/sqlite.py
@@ -1,6 +1,7 @@
 # author: Linhengyang
 import sqlite3
-
+import sys
+print(sys.executable)
 # search in DB
 def get_salary_in(low, high):
     try:
@@ -19,22 +20,22 @@ def get_salary_in(low, high):
         conn.close()
     return [tp[0] for tp in outputs]
 
-# try to create table and insert records
-conn = sqlite3.connect(r"../Data/testDB.db")
-cursor = conn.cursor()
-# create one table
-sql = "CREATE TABLE USER (ID INT PRIMARY KEY NOT NULL, NAME CHAR(20), SCORE INT)"
-cursor.execute(sql)
-# insert one record to USER table
-sql = "INSERT INTO USER VALUES (?, ?, ?)"
-cursor.execute(sql, (1, "Aaron", 99))
-cursor.rowcount
-# insert another record to USER table
-cursor.execute(sql, (2, "Hengyang", 100))
-cursor.rowcount
-# insert multiple records to USER table by executemany(sql, seq_of_params)
-cursor.executemany(sql, ((3, "Lin", 100), (4, "other", 50)))
+# # try to create table and insert records
+# conn = sqlite3.connect(r"../Data/testDB.db")
+# cursor = conn.cursor()
+# # create one table
+# sql = "CREATE TABLE USER (ID INT PRIMARY KEY NOT NULL, NAME CHAR(20), SCORE INT)"
+# cursor.execute(sql)
+# # insert one record to USER table
+# sql = "INSERT INTO USER VALUES (?, ?, ?)"
+# cursor.execute(sql, (1, "Aaron", 99))
+# cursor.rowcount
+# # insert another record to USER table
+# cursor.execute(sql, (2, "Hengyang", 100))
+# cursor.rowcount
+# # insert multiple records to USER table by executemany(sql, seq_of_params)
+# cursor.executemany(sql, ((3, "Lin", 100), (4, "other", 50)))
 
-cursor.close()
-conn.commit()
-conn.close()
+# cursor.close()
+# conn.commit()
+# conn.close()
